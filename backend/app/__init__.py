502f4c53128516355ecdb39aa49cc3e68043e382
Moigh <moigh777@hotmail.com>
2026-01-15 11:48:17 +1000

    Flask test

---
+++ backend/app/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Backend is working!'
+
+@app.route('/api/test')
+def test():
+    return 'Test is working!'
