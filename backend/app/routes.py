5f2caf71b4fc95a01b593ce82ff548211c11d963
Moigh <moigh777@hotmail.com>
2026-01-16 18:37:27 +1000

    added all routes

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -21,6 +21,27 @@ def get_comic_chapters(comic_id):
         return jsonify([chapter.to_dict() for chapter in comic.chapters])
     return jsonify({'error': 'comic not found'}), 404
 
+# Получить ОДНУ главу
+def get_chapter(chapter_id):
+    chapter = Chapter.query.get(chapter_id)
+    if chapter:
+        return jsonify(chapter.to_dict())
+    return jsonify({'error': 'chapter not found'}), 404
+
+# Получить страницы главы
+def get_chapter_pages(chapter_id):
+    chapter = Chapter.query.get(chapter_id)
+    if chapter:
+        return jsonify([page.to_dict() for page in chapter.pages])
+    return jsonify({'error': 'chapter not found'}), 404
+
+# Получить ОДНУ страницу
+def get_page(page_id):
+    page = Page.query.get(page_id)
+    if page:
+        return jsonify(page.to_dict())
+    return jsonify({'error': 'page not found'}), 404
+
 # Заполнить БД тестовыми данными
 def populate_db():
     db.drop_all()
