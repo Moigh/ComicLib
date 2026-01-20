3565c9feaa5ab35eaeea5cce96a26232e0bcf69f
Moigh <moigh777@hotmail.com>
2026-01-20 16:09:38 +1000

    first App.js test page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,50 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function HomePage() {
-  return <h1>Главная страница</h1>;
-}
-
-function AboutPage() {
-  return <h1>О проекте</h1>;
-}
-
 function App() {
   return (
-    <Router>
-      <nav>
-        <Link to="/">Главная</Link> | 
-        <Link to="/about">О проекте</Link>
-      </nav>
-      
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-      </Routes>
-    </Router>
-  );
-}
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <h1>Hello React!</h1>
+      <p>Мой первый компонент работает!</p>
     </div>
   );
 }
