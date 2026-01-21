f47886e98a3d8a32ada029b5011c0e0a2f571b6c
Moigh <moigh777@hotmail.com>
2026-01-21 18:03:13 +1000

    frontend base structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Home() {
+  return <h1>Домашняя</h1>;
+}
+
+function About() {
+  return <h1>О проекте</h1>;
+}
 
 function App() {
   return (
-    <div className="container mt-5">
-      <h1>Hello React!</h1>
-      <p>Мой первый компонент работает!</p>
-    </div>
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Главная</Link> | 
+        <Link to="/about">О нас</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
