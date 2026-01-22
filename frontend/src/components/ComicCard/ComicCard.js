23dd0fc4c3a9efa8db8e3d72dbcfe4072f6d3cdb
Moigh <moigh777@hotmail.com>
2026-01-22 14:44:47 +1000

    sep comiccard css styles

--- frontend/src/components/ComicCard/ComicCard.js
+++ frontend/src/components/ComicCard/ComicCard.js
@@ -1,48 +1,32 @@
+import styles from './ComicCard.module.css'; 
+
 function ComicCard({ comic }) {
   return (
     <div 
-    key={comic.id} 
-		className="col-6 col-sm-4 col-md-3 col-lg-2"
+    key = {comic.id} 
+		className = "col-6 col-sm-4 col-md-3 col-lg-2"
 		>
-			<div className="card h-100 border-0">
-				<div 
-				style={{
-					position: 'relative',
-					paddingTop: '150%', 
-					overflow: 'hidden'
-				}}
-				>
+			<div className = "card h-100 border-0">
+				<div className = {`${styles.body}`}>
 				<img 
-					src={comic.cover_image}
-					alt={comic.name}
-					style={{
-					position: 'absolute',
-					top: 0,
-					left: 0,
-					width: '100%',
-					height: '100%',
-					objectFit: 'cover'
-					}}
+					src = {comic.cover_image}
+					alt = {comic.name}
+					className = {`${styles.image}`}
 				/>
 				</div>
 				
-				<div className="card-body p-1">
+				<div className = "card-body p-1">
 					<h6 
-						className="card-title " 
-						style={{
-						fontSize: '0.85rem',
-						height: '2rem',
-						overflow: 'hidden'
-						}}
+						className = {`card-title ${styles.title}`} 
 					>
 						{comic.name}
 					</h6>
 					
-					<div className="d-flex justify-content-between">
-						<small className="text-muted" style={{ fontSize: '0.75rem' }}>
+					<div className = "d-flex justify-content-between">
+						<small className = {`text-muted ${styles.text}`}>
 						{comic.year}
 						</small>
-						<small className="text-muted" style={{ fontSize: '0.75rem' }}>
+						<small className = {`text-muted ${styles.text}`}>
 						Глав: {comic.chapters_count}
 						</small>
 					</div>
