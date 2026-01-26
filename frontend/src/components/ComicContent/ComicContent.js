995bcd67b9669abb38677f8f2e4cecf5defb4e58
Moigh <moigh777@hotmail.com>
2026-01-26 11:33:02 +1000

    ComicContent sep to component h1

--- frontend/src/components/ComicContent/ComicContent.js
+++ frontend/src/components/ComicContent/ComicContent.js
@@ -3,7 +3,20 @@ import { Link } from 'react-router-dom';
 
 function ComicContent( {comic, chapters}) {
   return (
-		<><div className="mb-5">
+		<>		
+		<h1 className="mb-3">{comic.name}</h1>
+		
+		<div className="mb-4">
+			<div className="d-flex flex-wrap gap-2 mb-2">
+				{comic.authors?.map((author, idx) => (
+					<span key={idx} className="badge bg-primary">
+						{author.name}
+					</span>
+				))}
+			</div>
+			<div className="text-muted"> {comic.year}</div>
+		</div>
+		<div className="mb-5">
 			<h4>Описание</h4>
 			<p className="fs-5">{comic.description}</p>
 		</div><div className="mb-5">
