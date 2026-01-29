6be587ad1ab1039624c3ec0a22ae037d7f822657
Moigh <moigh777@hotmail.com>
2026-01-29 10:49:18 +1000

    production

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,10 +1,18 @@
+import os
+from pathlib import Path
 from flask import Flask
 from .models import db 
 from . import routes
 from flask_cors import CORS
+from flask import send_from_directory
+
+
+# Получаем путь к build 
+frontend_build =(Path(__file__).parent.parent.parent / 'frontend' / 'build').resolve()
 
 app = Flask(__name__)
 CORS(app, origins=['http://localhost:3000'])
+
 
 # КОНФИГУРАЦИЯ БАЗЫ ДАННЫХ
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comicbooks.db'
@@ -17,7 +25,7 @@ db.init_app(app)
 with app.app_context():
     db.create_all()
 
-# Регистрируем маршруты
+# API
 @app.route('/api/comics', methods=['GET'])
 def get_comics():
     return routes.get_comics()
@@ -42,11 +50,11 @@ def get_chapter_pages(chapter_id):
 def get_page(page_id):
     return routes.get_page(page_id)
 
-# Тестовые маршруты
-@app.route('/')
-def hello():
-    return 'Backend is working!'
+# Production
+@app.route('/static/<path:filename>')
+def find_static(filename):
+    return send_from_directory(os.path.join(app.frontend_build, 'static'), filename)
 
-@app.route('/api/test')
-def test():
-    return 'Test is working!'
+@app.route('/<path:path>')
+def serve_react(path):
+    return send_from_directory(app.frontend_build, 'index.html')
