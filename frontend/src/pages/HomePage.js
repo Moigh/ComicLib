f58d57ee611dde3f16c3aeaa42c8569df3ab2e36
Moigh <moigh777@hotmail.com>
2026-01-29 19:13:13 +1000

    localhost back

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -9,7 +9,7 @@ function HomePage() {
     setLoading(true);
     
     // Запрос к API
-    fetch('http://backend:5000/api/comics')
+    fetch('http://localhost:5000/api/comics')
       .then(response => response.json())
       .then(data => {
         setComics(data);
